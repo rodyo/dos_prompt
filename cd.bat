2237ba1cd5ff63d87a0338688961590b9d530f07
Rody Oldenhuis <oldenhuis@gmail.com>
2014-10-28 11:01:01 +0100

    Fixed some issues regarding dirnames with spaces in them

--- cd.bat
+++ cd.bat
@@ -1,6 +1,6 @@
 @echo off
 
-REM no arguments (go to home dir)
+:: no arguments (go to home dir)
 set argc=0
 for %%X in (%*) do set /A argc+=1
 if %argc%==0 (
@@ -8,23 +8,22 @@ if %argc%==0 (
     goto finish
 )
 
-
-REM minus (go to previous dir)
-if %~1==- (
+:: minus (go to previous dir)
+if "%~1"=="-" (
     popd
     goto finish
 )
 
 
-REM execute command with tilde-expansion (replace with home dir)
-for /f "tokens=* delims=" %%A in ('call tilde_expansion ^%*') do set argv=%%A
+:: execute command with tilde-expansion (replace with home dir)
+for /f "tokens=* delims=" %%A in ('call tilde_expansion %*') do set "argv=%%A"
 pushd %argv%
 goto finish
 
 
 :finish
 if errorlevel==0 (
-    title %cd%
+    title "%cd%"
     cls
     dir /og
     exit /b 0
